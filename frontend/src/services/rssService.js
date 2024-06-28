5ba2250032f4ab3b5de8a54b6293c8a8b9f45a89
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-06-28 21:27:03 +0300

    fetch articles

--- frontend/src/services/rssService.js
+++ frontend/src/services/rssService.js
@@ -2,9 +2,8 @@ import axios from 'axios';
 
 export const fetchRSSFeed = async (url) => {
     try {
-        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/webparser`, { url });
+        const response = await axios.get(`https://api.rss2json.com/v1/api.json?rss_url=${url}`);
         return response.data;
-
     } catch (error) {
         console.error("Error fetching the RSS feed", error);
         throw error;
